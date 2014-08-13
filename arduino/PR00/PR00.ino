5d5d052678ddcf3754ad49c04512e966eea410d1
Alexis Schad <schadoc_alex@hotmail.fr>
2014-08-13 12:18:37 +0200

    Modifs mineures code Arduino

--- arduino/PR00/PR00.ino
+++ arduino/PR00/PR00.ino
@@ -14,10 +14,28 @@
 #include <XBee.h>
 #include "constants.h"
 
+// Variables
+XBee xbee = XBee();
+Rx16Response rx16 = Rx16Response();
+uint8_t* data = 0;
+
 void setup() {
     Serial1.begin(BAUDRATE_XBEE);
+    xbee.setSerial(Serial1);
+
+    // DEBUG
+    Serial.begin(9600);
 }
 
 void loop() {
+    xbee.readPacket();
+
+    if (xbee.getResponse().getApiId() == RX_16_RESPONSE) {
+        xbee.getResponse().getRx16Response(rx16);
+        data = rx16.getData();
+    }
+
+    Serial.print(data);
+
     delay(1000);
 }
