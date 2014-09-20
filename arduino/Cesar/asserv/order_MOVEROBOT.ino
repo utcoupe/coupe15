f4d72e983e54d168ce45fed740af4cb1fb4eac79
Alexis Schad <schadoc_alex@hotmail.fr>
2014-09-20 19:17:52 +0200

    Tentative télécommande pour César

---
+++ arduino/Cesar/asserv/order_MOVEROBOT.ino
@@ -0,0 +1,119 @@
+/*
+  ╦ ╦╔╦╗╔═╗┌─┐┬ ┬┌─┐┌─┐
+  ║ ║ ║ ║  │ ││ │├─┘├┤ 
+  ╚═╝ ╩ ╚═╝└─┘└─┘┴  └─┘
+  │ AFMotor.ino
+  └────────────────────
+
+  Adafruit Motor shield library
+  copyright Adafruit Industries LLC, 2009
+  this code is public domain, enjoy!
+  Adapté pour UTCoupe2011 par Arthur, 19/01/2011
+  Moteurs 1 et 2 correspondent à la carte Rugged
+
+  Author(s)
+    - Alexis Schad : schadoc_alex@hotmail.fr
+*/
+
+#include "brushlessMotor.h"
+#include "order_MOVEROBOT.h"
+
+BrushlessMotor motor_left(MOTOR_LEFT);
+BrushlessMotor motor_right(MOTOR_RIGHT);
+
+static int current_pwm = 0;
+static int current_pwm_left = 0;
+static bool status[4] = {false,false,false,false};
+
+static unsigned long previous_time;
+
+static float acceleration = 0.010; // PWM / secondes (1000 ms)
+static int PMW_MAX = 128;
+
+void initOrder_MOVEROBOT() {
+	set_pwm_right(127);
+	set_pwm_left(127);
+	previous_time = millis();
+}
+
+void executeOrder_MOVEROBOT(int type, int dir) {
+	bool new_state;
+	if(type == KPDOWN)
+		new_state = true;
+	else
+		new_state = false;
+
+	status[dir] = new_state;
+}
+
+void loopOrder_MOVEROBOT() {
+  	unsigned long current_time = millis();
+	int i;
+	unsigned long delay_time = current_time - previous_time;
+        
+	if(status[KUP])
+		current_pwm += int(float(delay_time) * acceleration);
+	else if(status[KDOWN])
+		current_pwm -= int(float(delay_time) * acceleration);
+        else
+                current_pwm = 0;
+	if(current_pwm > PMW_MAX) current_pwm = PMW_MAX;
+	if(current_pwm < -PMW_MAX) current_pwm = -PMW_MAX;
+
+	if(status[KLEFT])
+		current_pwm_left += int(float(delay_time) * acceleration);
+	else if(status[KRIGHT])
+		current_pwm_left -= int(float(delay_time) * acceleration);
+        else
+                current_pwm_left = 0;
+	if(current_pwm_left > PMW_MAX*2) current_pwm_left = PMW_MAX*2;
+	if(current_pwm_left < -PMW_MAX*2) current_pwm_left = -PMW_MAX*2;
+
+	if(current_pwm_left >= 0) {
+		set_pwm_right(current_pwm + current_pwm_left);
+		set_pwm_left(current_pwm - current_pwm_left);
+	}
+	else {
+		set_pwm_right(current_pwm + current_pwm_left);
+		set_pwm_left(current_pwm - current_pwm_left);
+	}
+
+	previous_time = current_time;
+}
+
+void set_pwm_left(int pwm){
+	if (pwm != NO_PWM) {
+		pwm = -pwm;//les moteurs sont faces à face, pour avancer il faut qu'il tournent dans un sens différent
+		if (pwm > 0)
+			pwm += PWM_MIN;
+		else if (pwm < 0)
+			pwm -= PWM_MIN;
+
+		pwm += 127;
+
+		if(pwm > 255)
+			pwm = 255;
+		else if(pwm < 0)
+			pwm = 0;
+	}
+	motor_left.setPwm(666);
+}
+
+void set_pwm_right(int pwm) {
+	if (pwm != NO_PWM) {
+		if (pwm > 0)
+			pwm += PWM_MIN;
+		else if (pwm < 0)
+			pwm -= PWM_MIN;
+
+		pwm += 127;
+
+		if(pwm > 255)
+			pwm = 255;
+		else if(pwm < 0)
+			pwm = 0;
+	}
+	motor_right.setPwm(666);
+}
+
+
