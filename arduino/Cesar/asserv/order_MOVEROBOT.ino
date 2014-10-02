238a7160a7f186e1e1630f440fa47ab259cc7b25
Alexis Schad <schadoc_alex@hotmail.fr>
2014-10-02 20:00:10 +0200

    Télécommande robot fonctionnelle

--- arduino/Cesar/asserv/order_MOVEROBOT.ino
+++ arduino/Cesar/asserv/order_MOVEROBOT.ino
@@ -27,7 +27,7 @@ static bool status[4] = {false,false,false,false};
 
 static unsigned long previous_time;
 
-static float acceleration = 0.010; // PWM / secondes (1000 ms)
+static float acceleration = 0.100; // PWM / secondes (1000 ms)
 static int PMW_MAX = 128;
 
 void initOrder_MOVEROBOT() {
@@ -46,16 +46,30 @@ void executeOrder_MOVEROBOT(int type, int dir) {
 }
 
 void loopOrder_MOVEROBOT() {
-  	/*unsigned long current_time = millis();
+  	unsigned long current_time = millis();
 	int i;
 	unsigned long delay_time = current_time - previous_time;
-        
+    if(delay_time > 30) {
 	if(status[KUP])
 		current_pwm += int(float(delay_time) * acceleration);
 	else if(status[KDOWN])
 		current_pwm -= int(float(delay_time) * acceleration);
+    /*else if(!status[KUP] && !status[KDOWN]) {
+          // Décélération
+          if(current_pwm > 0) {
+            current_pwm -= int(float(delay_time) * acceleration * 2);
+            if(current_pwm < 0)
+              current_pwm = 0;
+          }
+          else if(current_pwm < 0) {
+            current_pwm += int(float(delay_time) * acceleration * 2);
+            if(current_pwm > 0)
+              current_pwm = 0;
+          }
+        }
+    */
         else
-                current_pwm = 0;
+          current_pwm = 0;
 	if(current_pwm > PMW_MAX) current_pwm = PMW_MAX;
 	if(current_pwm < -PMW_MAX) current_pwm = -PMW_MAX;
 
@@ -63,25 +77,19 @@ void loopOrder_MOVEROBOT() {
 		current_pwm_left += int(float(delay_time) * acceleration);
 	else if(status[KRIGHT])
 		current_pwm_left -= int(float(delay_time) * acceleration);
-        else
-                current_pwm_left = 0;
+    else
+        current_pwm_left = 0;
 	if(current_pwm_left > PMW_MAX*2) current_pwm_left = PMW_MAX*2;
 	if(current_pwm_left < -PMW_MAX*2) current_pwm_left = -PMW_MAX*2;
 
-	if(current_pwm_left >= 0) {
-		set_pwm_right(current_pwm + current_pwm_left);
-		set_pwm_left(current_pwm - current_pwm_left);
-	}
-	else {
-		set_pwm_right(current_pwm + current_pwm_left);
-		set_pwm_left(current_pwm - current_pwm_left);
-	}
+	set_pwm_left(current_pwm + current_pwm_left);
+	set_pwm_right(current_pwm - current_pwm_left);
 
-	previous_time = current_time;*/
+	previous_time = current_time;
+    }
 }
 
 void set_pwm_left(int pwm){
-        pwm = -pwm;
 	if (pwm != NO_PWM) {
 		pwm += 127;
 
@@ -101,6 +109,8 @@ void set_pwm_right(int pwm) {
 			pwm = 255;
 		else if(pwm < 0)
 			pwm = 0;
+
+		pwm = 255 - pwm; // Moteur pas dans le même sens
 	}
 	motor_right.setPwm(pwm);
 }
