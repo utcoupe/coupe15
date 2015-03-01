1762c12c43ea09b57d225412db255dedf96a6074
Mewen <mewenmichel@yahoo.fr>
2015-03-01 18:59:00 +0100

    Comm + télécommande avec Hokuyo

--- hokuyo/client_hok.js
+++ hokuyo/client_hok.js
@@ -8,24 +8,25 @@
 
 	var log4js = require('log4js');
 	var logger = log4js.getLogger('Client');
-	var util = require("util"); // var_dump like functions
 	var child_process = require('child_process');
 	var child;
-	var SocketClient = require('client.class.js');
+	var SocketClient = require('../server/socket_client.class.js');
 
 	var ourPositions = {};
 
-	var server = "http://172.25.7.186:3128"; // server adress
+	var server = ""; // server adress
 	var client = new SocketClient({
 		server_ip: server,
 		type: "hokuyo"
 	});
 
 	client.order(function(from, name, params){
+		logger.info("Just received an order `" + name + "` from " + from + " with params :");
+		logger.info(params);
 		switch (name){
 			case "start":
 				if(!!params.color && !!params.nbrobots)
-					start();
+					start(params.color, params.nbrobots);
 				break;
 			case "stop":
 				if(!!child)
@@ -33,7 +34,6 @@
 				break;
 			default:
 				logger.warn("Name not understood : " + data);
-				break;
 		}
 	});
 
@@ -46,14 +46,14 @@
 		//do something when app is closing
 		process.on('exit', quitC);
 		//catches ctrl+c event
-		process.on('SIGINT', quitC);
+		// process.on('SIGINT', quitC);
 		//catches uncaught exceptions
 		process.on('uncaughtException', quitC);
 
 		// Functions
 		function parseRobots(string) {
 			var dots = string.split("#");
-			for (var i = dots.length - 1; i >= 0; i--) {
+			for (var i = 0; i <= dots.length - 1; i++) {
 				dots[i] = dots[i].split(",");
 				dots[i][0] = parseInt(dots[i][0]);
 				dots[i][1] = parseInt(dots[i][1]);
@@ -77,7 +77,6 @@
 					break;
 				default:
 					logger.info("Error not understood : " + string);
-					break;
 			}
 		}
 
@@ -85,23 +84,24 @@
 			// input format (XXXX type and xxxx values) : "[XXXX]xxxxxxxxx" maybe many times, seperated with \n
 
 			var inputAr = input.toString().split('\n');
-
-			for (var i = inputAr.length - 1; i >= 0; i--) {
-				switch (inputAr[i].substring(1,5)){
-					case "HI:)":
-						// XXX send "C started" to server
-						logger.info('C Hokuyo software says "Hi !" :)');
-						// client.send("IA", "nb_hokuyo", {nb: 2});
-						break;
-					case "DATA":
-						parseRobots(inputAr[i].substring(6));
-						break;
-					case "WARN":
-						parseError(inputAr[i].substring(6));
-						break;
-					default:
-						logger.info("Data not understood at line " + i + " : " + input);
-						break;
+			
+			for (var i = 0; i <= inputAr.length - 1; i++) {
+				if (!!inputAr[i]){
+					switch (inputAr[i].substring(1,5)){
+						case "HI:)":
+							// XXX send "C started" to server
+							logger.info('C Hokuyo software says "Hi !" :)');
+							// client.send("IA", "nb_hokuyo", {nb: 2});
+							break;
+						case "DATA":
+							parseRobots(inputAr[i].substring(6));
+							break;
+						case "WARN":
+							parseError(inputAr[i].substring(6));
+							break;
+						default:
+							logger.info("Data "+ inputAr[i].substring(1,5) + "not understood at line " + i + " : " + inputAr[i]);
+					}
 				}
 			};
 		}
@@ -112,9 +112,7 @@
 
 		// Execute C program
 		var command = "./bin/hokuyo";
-		var init_wizard = 'no_init_wizard'; // idem
-		var nbr_hok = '4';
-		var args = [color, init_wizard, nbrobots];
+		var args = [color, 'no_init_wizard', nbrobots];
 		// var options = // default : { cwd: undefined, env: process.env};
 		logger.info('Launching : ' + command + ' ' + args);
 		child = child_process.spawn(command, args);
@@ -125,11 +123,14 @@
 		});
 
 		child.stderr.on('data', function(data) {
-			logger.info('stderr: ' + data);
+			logger.error(data);
 		});
 		
 		child.on('close', function(code) {
-			logger.info('\nChild closed with code: ' + code);
+			if (code == 1)
+				logger.info('Child closed correctly');
+			else
+				logger.info('Child closed with code: ' + code);
 			// Send message XXX
 		});
 	};
--- webclient/pages/telecommande/telecommande.js
+++ webclient/pages/telecommande/telecommande.js
@@ -1,3 +1,14 @@
 angular.module('app').controller('TelecommandeCtrl', ['$scope', function($scope) {
-	
+
+	$(document).on("click", "#rc_hok_start", function(e) {
+		client.send("hokuyo", "start", {
+			"color": "green",
+			"nbrobots": 4});
+		console.log("Message `start` sent");
+	});
+
+	$(document).on("click", "#rc_hok_stop", function(e) {
+		client.send("hokuyo", "stop", {});
+		console.log("Message `stop` sent");
+	});
 }]);
