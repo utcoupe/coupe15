775df086a3320493699ff46b93f75967d5a7f6d9
mmewen <mewenmichel@yahoo.fr>
2015-05-02 00:33:58 +0200

    Cette fois c'est la bonne...

--- hokuyo/main.sh
+++ hokuyo/main.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "Launching Hokuyo Node.JS app..."
-sudo node /home/pi/coupe15/client_hok.js &
+# echo "Launching Hokuyo Node.JS app..."
+sudo node /home/pi/coupe15/hokuyo/client_hok.js &
